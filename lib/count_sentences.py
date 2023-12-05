1b170b69bc278f766a279f70e213c68fa06f0d0f
Samuel Gichuhi <gichuhi1sam@gmail.com>
2023-12-06 02:31:42 +0300

    Completed lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,43 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  
+  def __init__(self,value=""):
+    self.value=value
+  
+  def get_value(self):
+    return self._value
+
+  def set_value(self,value):
+    if type(value)==str:
+      self._value=value
+    else:
+      print("The value must be a string.")
+
+  value=property(get_value,set_value)
+
+  def is_sentence(self):
+    return self.value.endswith(".")
+
+  def is_question(self):
+    return self.value.endswith("?")
+  
+  def is_exclamation(self):
+    return self.value.endswith("!")
+  
+  def count_sentences(self):
+    
+    new_value=self.value.replace("!",".")
+    new_value=new_value.replace("?",".")
+
+    count_list=[x for x in new_value.split(".") if x != ""]
+    print(count_list)
+    return len(count_list)
+  
+test = MyString("This lab. Will Definetly be! Annoying to do?")
+print(test.is_question())
+print(test.count_sentences())
+
+
+
+
